5bf92ecb461fe1743607b68fc3a18a1c50161404
Crowdbotics <team@crowdbotics.com>
2023-08-13 20:18:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-bird-42830",
+  url: "https://dry-bird-42830.botics.co",
+  api: "https://dry-bird-42830.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
